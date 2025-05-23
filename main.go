a650457697cab9a715351b98a70e339ba46b3206
Ayrton Vitor <ayrtonvitor@pm.me>
2025-05-22 23:56:27 -0300

    read file in 8 bytes chunks

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("./messages.txt")
+	if err != nil {
+		log.Fatal("Could not open file: %w", err)
+	}
+	defer file.Close()
+
+	chunk := make([]byte, 8)
+	for {
+		n, err := file.Read(chunk)
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				log.Fatal("Error reading from file: %w", err)
+			}
+			break
+		}
+
+		fmt.Printf("read: %s\n", string(chunk[:n]))
+	}
 }
