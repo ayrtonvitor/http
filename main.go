c2ed246fa52d3eecd9883663032595dede85d5a0
Ayrton Vitor <ayrtonvitor@pm.me>
2025-05-23 03:12:48 -0300

    move line read to a new func

--- main.go
+++ main.go
@@ -16,24 +16,38 @@ func main() {
 	}
 	defer file.Close()
 
-	line := ""
-	for {
-		buf := make([]byte, 8)
-		n, err := file.Read(buf)
-		if err != nil {
-			if !errors.Is(err, io.EOF) {
-				log.Fatal("Error reading from file: %w", err)
-			}
-			if line != "" {
-				fmt.Printf("read: %s%d\n", line, len(line))
-			}
-			break
-		}
-		parts := strings.Split(string(buf[:n]), "\n")
-		for _, part := range parts[:len(parts)-1] {
-			fmt.Printf("read: %s%s\n", line, part)
-			line = ""
-		}
-		line += string(parts[len(parts)-1])
+	linesChan := getLineChannel(file)
+	for line := range linesChan {
+		fmt.Printf("read: %s\n", line)
 	}
+
+}
+
+func getLineChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer close(lines)
+		line := ""
+		for {
+			buf := make([]byte, 8)
+			n, err := f.Read(buf)
+			if err != nil {
+				if !errors.Is(err, io.EOF) {
+					log.Fatal("Error reading from file: %w", err)
+				}
+				if line != "" {
+					lines <- line
+				}
+				return
+			}
+			parts := strings.Split(string(buf[:n]), "\n")
+			for _, part := range parts[:len(parts)-1] {
+				lines <- line + part
+				line = ""
+			}
+			line += string(parts[len(parts)-1])
+		}
+	}()
+
+	return lines
 }
