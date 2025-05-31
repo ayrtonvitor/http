3c358868433f59fc9d437fd9a42abbf6814de241
Ayrton Vitor <ayrtonvitor@pm.me>
2025-05-31 14:34:49 -0300

    read request as stream

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,8 +1,8 @@
 package request
 
 import (
+	"bytes"
 	"errors"
-	"fmt"
 	"io"
 	"slices"
 	"strings"
@@ -10,6 +10,7 @@ import (
 
 type Request struct {
 	RequestLine RequestLine
+	state       reqState
 }
 
 type RequestLine struct {
@@ -18,32 +19,101 @@ type RequestLine struct {
 	Method        string
 }
 
+type reqState = int
+
+const (
+	reqStateInitialized reqState = iota
+	reqStateDone
+)
+
 const crlf = "\r\n"
+const bufSize = 8
+
+var errUnknownReqState = errors.New("Unknown state")
+var errProcReqInDoneState = errors.New("Trying to read data in done state")
 
 var ErrMalformedReq = errors.New("Request is malformed")
 var ErrUnknownVerb = errors.New("HTTP verb is invalid")
 var ErrUnsupportedVersion = errors.New("HTTP version is not supported")
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	rawReq, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, fmt.Errorf("Error reading request: %w\n", err)
+	req := &Request{
+		state: reqStateInitialized,
 	}
-	reqLine, err := parseRequestLine(string(rawReq))
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing request line: %w\n", err)
+	buf := make([]byte, bufSize)
+	nRead := 0
+
+	for req.state != reqStateDone {
+		if nRead >= len(buf) {
+			buf = append(buf, make([]byte, len(buf))...)
+		}
+
+		nReadNew, err := reader.Read(buf[nRead:])
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				req.state = reqStateDone
+				break
+			}
+			return nil, err
+		}
+		nRead += nReadNew
+
+		nParsed, err := req.parse(buf)
+		if err != nil {
+			return nil, err
+		}
+
+		if nParsed > 0 {
+			copy(buf, buf[nParsed:])
+			nRead -= nParsed
+		}
 	}
-	return &Request{
-		RequestLine: *reqLine,
-	}, nil
+	return req, nil
 }
 
-func parseRequestLine(rawReq string) (*RequestLine, error) {
-	parts := strings.Split(rawReq, crlf)
-	if len(parts) < 3 {
-		return nil, ErrMalformedReq
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.state {
+	case reqStateInitialized:
+		return r.parseInitReq(data)
+	case reqStateDone:
+		return 0, errProcReqInDoneState
+	default:
+		return 0, errUnknownReqState
 	}
-	reqLineParts := strings.Split(parts[0], " ")
+}
+
+func (r *Request) parseInitReq(data []byte) (int, error) {
+	reqLine, n, err := parseRequestLine(data)
+	if err != nil {
+		return 0, err
+	}
+
+	if n == 0 {
+		return 0, err
+	}
+
+	r.state = reqStateDone
+	r.RequestLine = *reqLine
+	return n, nil
+}
+
+func parseRequestLine(rawReq []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(rawReq, []byte(crlf))
+	if idx < 0 {
+		return nil, 0, nil
+	}
+
+	rawReqLine := string(rawReq[:idx])
+	reqLine, err := requestLineFromString(rawReqLine)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return reqLine, idx + len(crlf), nil
+}
+
+func requestLineFromString(str string) (*RequestLine, error) {
+	reqLineParts := strings.Split(str, " ")
 	if len(reqLineParts) != 3 {
 		return nil, ErrMalformedReq
 	}
@@ -57,9 +127,11 @@ func parseRequestLine(rawReq string) (*RequestLine, error) {
 	}
 	version := "1.1"
 
-	return &RequestLine{
+	requestLine := &RequestLine{
 		HttpVersion:   version,
 		RequestTarget: reqLineParts[1],
 		Method:        reqLineParts[0],
-	}, nil
+	}
+
+	return requestLine, nil
 }
