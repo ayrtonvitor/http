aa657af70e0c188ed2c5d8733c999c092570de04
Ayrton Vitor <ayrtonvitor@pm.me>
2025-06-01 22:29:22 -0300

    test request parsing with simple malformed header

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -85,6 +85,14 @@ func TestParsingRequestHeaders(t *testing.T) {
 	assert.Equal(t, "localhost:42069", r.Headers["host"])
 	assert.Equal(t, "curl/7.81.0", r.Headers["user-agent"])
 	assert.Equal(t, "*/*", r.Headers["accept"])
+
+	// Test: Malformed Header
+	reader = &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost : localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err = RequestFromReader(reader)
+	require.Error(t, err)
 }
 
 type chunkReader struct {
